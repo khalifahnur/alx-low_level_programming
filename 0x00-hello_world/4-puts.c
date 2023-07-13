a2ffa92463697bea857491138e3e67524d6a79a1
khalifahnur <khalifahnur1095@gmail.com>
2023-07-13 08:43:31 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main:outputs the information using puts
+ * return:null
+ *
+ * the program outputs the information using puts keyword
+ */
 int main(void)
 {
+	/*declare the function*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - outputs the function
+ * return: 0(success)
+ */
+int main(void)
+/*starting poitn of main function8*/
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
