3a1a4d8e141b7816ae82ac9339818c8e90028bad
khalifahnur <khalifahnur1095@gmail.com>
2023-07-13 05:11:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-
-/**
- * this file is used to output the code
- * using the puts keyword 
- * that all
- */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
