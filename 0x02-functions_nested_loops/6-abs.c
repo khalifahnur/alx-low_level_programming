c091288b98e1bf47541c0443b8d8ebdde5b4a3a3
khalifahnur <khalifahnur1095@gmail.com>
2023-07-18 07:01:08 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - Check main
+ * @n: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
