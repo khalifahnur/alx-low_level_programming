94f76acda7ff2a59f81d2bed5dbaff6559370c9d
khalifahnur <khalifahnur1095@gmail.com>
2023-07-18 04:00:20 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Check description
+ * Description: It prints the _putchar, followed by a new line.
+ * Return: 0 (success).
+ */
+int main(void) {
+    printf("_putchar\n");
+    return 0;
+}
+
