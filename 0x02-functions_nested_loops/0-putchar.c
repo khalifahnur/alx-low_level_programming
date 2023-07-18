0fbe30b360ed604e8f2aa6bfcced330a5ea2a392
khalifahnur <khalifahnur1095@gmail.com>
2023-07-18 04:36:30 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,8 @@
  * Description: It prints the _putchar, followed by a new line.
  * Return: 0 (success).
  */
-int main(void) {
-    printf("_putchar\n");
-    return 0;
+int main(void)
+{
+	pritf("_putchar\n");
+	return (0);
 }
-
