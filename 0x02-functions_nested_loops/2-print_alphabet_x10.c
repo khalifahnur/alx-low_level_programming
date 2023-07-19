619c65808feb374a5eaa3ed6b49e1e1fd48495fe
khalifahnur <khalifahnur1095@gmail.com>
2023-07-19 08:43:32 -0700

    natural

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 
+ * print_alphabet_x10: see main.h 
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: 0 (success)
  */
-
 void print_alphabet_x10(void)
 {
 	char alph;
